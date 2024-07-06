89c5adc89fd58dc186853bf6414e943a8bbe8d9a
Santosh kc <santoshkc7786@gmail.com>
2024-07-06 22:10:59 +0545

    Bug Fixes

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import { Separator } from "@/components/ui/separator";
-import React from "react";
+import React, { FormEvent, useRef } from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(emailRef.current?.value, passwordRef.current?.value);
+  };
   return (
     <div className="h-full  overflow-y-hidden w-screen flex justify-center items-center">
       <div className=" flex flex-col sm:w-3/4 sm:px-5 md:w-1/2 lg:w-1/3 xl:w-1/4  py-5 w-full  mx-4 rounded-xl bg-stone-200 ">
@@ -28,7 +36,7 @@ const page = (props: Props) => {
           <Separator className="bg-stone-400 w-[90%]  " />
         </div>
         <div className="mx-2">
-          <form action="" className="flex flex-col">
+          <form onSubmit={handleSubmit} className="flex flex-col">
             <label
               htmlFor="email"
               className="px-3 my-1 font-bold text-stone-600"
@@ -36,6 +44,7 @@ const page = (props: Props) => {
               Email Address
             </label>
             <input
+              ref={emailRef}
               type="email"
               name="email"
               className="h-10 px-3 rounded-xl"
@@ -49,6 +58,7 @@ const page = (props: Props) => {
             </label>
             <div className="relative w-full h-10 flex items-center   rounded-xl">
               <input
+                ref={passwordRef}
                 type="password"
                 name="password"
                 className="w-full h-full px-3 rounded-xl"
@@ -65,7 +75,7 @@ const page = (props: Props) => {
               </h2>
             </div>
             <input
-              type="button"
+              type="submit"
               value="Log In"
               className="p-3 my-3 text-xl font-bold text-white
               transition-colors duration-500 cursor-pointer rounded-xl bg-stone-500 hover:bg-stone-400
